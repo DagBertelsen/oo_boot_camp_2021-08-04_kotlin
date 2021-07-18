dccd9be4d86ddd7482d62e3a23adabbea27f9374
Fred George <fredgeorge@acm.org>
2021-07-18 16:03:25 +0200

    Fred: Initial commit of RectangleTest

---
+++ exercises/build.gradle.kts
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+ 
+plugins {
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    kotlin("jvm") version "1.5.21"
+
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
