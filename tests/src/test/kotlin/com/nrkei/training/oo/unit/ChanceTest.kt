fa18a3dc142fd7cdaf19b2851f44be5f27c806cd
Fred George <fredgeorge@acm.org>
2021-08-04 13:46:23 +0200

    Fred: Introduced convenience values for Chance testing

--- tests/src/test/kotlin/com/nrkei/training/oo/unit/ChanceTest.kt
+++ tests/src/test/kotlin/com/nrkei/training/oo/unit/ChanceTest.kt
@@ -7,18 +7,26 @@ import org.junit.jupiter.api.Test
 // Ensures Chance operates correctly
 internal class ChanceTest {
 
+    companion object {
+        private val IMPOSSIBLE = Chance(0)
+        private val UNLIKELY = Chance(0.25)
+        private val EQUALLY_LIKELY = Chance(0.5)
+        private val LIKELY = Chance(0.75)
+        private val CERTAIN = Chance(1)
+    }
+
     @Test
     fun equality() {
-        assertEquals(Chance(0.75), Chance(0.75))
-        assertNotEquals(Chance(0.75), Chance(0.25))
-        assertNotEquals(Chance(0.75), Any())
-        assertNotEquals(Chance(0.75), null)
+        assertEquals(LIKELY, Chance(0.75))
+        assertNotEquals(LIKELY, Chance(0.25))
+        assertNotEquals(LIKELY, Any())
+        assertNotEquals(LIKELY, null)
     }
 
     @Test
     fun sets() {
-        assertTrue(Chance(0.75) in hashSetOf(Chance(0.75)))
-        assertEquals(1, hashSetOf(Chance(0.75), Chance(0.75)).size)
+        assertTrue(LIKELY in hashSetOf(Chance(0.75)))
+        assertEquals(1, hashSetOf(LIKELY, Chance(0.75)).size)
     }
 
 }
