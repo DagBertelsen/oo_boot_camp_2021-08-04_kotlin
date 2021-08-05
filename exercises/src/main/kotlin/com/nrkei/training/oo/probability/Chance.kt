92c1c2caea42ea2a7b2b2543a21361cb426e8885
Fred George <fredgeorge@acm.org>
2021-08-05 08:33:10 +0200

    Fred: Chance handles rounding errors internally

--- exercises/src/main/kotlin/com/nrkei/training/oo/probability/Chance.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/probability/Chance.kt
@@ -1,17 +1,20 @@
 package com.nrkei.training.oo.probability
 
+import kotlin.math.absoluteValue
+
 class Chance(likelihoodAsFraction: Number) {
     companion object {
         private const val CERTAIN_FRACTION = 1.0
+        private const val EPSILON = 1e-10
     }
     private val fraction = likelihoodAsFraction.toDouble()
 
     override fun equals(other: Any?) =
         this === other || other is Chance && this.equals(other)
 
-    private fun equals(other: Chance) = this.fraction == other.fraction
+    private fun equals(other: Chance) = (this.fraction - other.fraction).absoluteValue < EPSILON
 
-    override fun hashCode() = fraction.hashCode()
+    override fun hashCode() = (fraction / EPSILON).toLong().hashCode()
 
     operator fun not() = Chance(CERTAIN_FRACTION - fraction)
 
