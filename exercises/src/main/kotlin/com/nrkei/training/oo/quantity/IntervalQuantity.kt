29b5ac6e011ba36da5a67c9f0e734dc77bc59452
Fred George <fredgeorge@acm.org>
2021-08-09 13:14:22 +0200

    Fred: Find the largest Quantity

--- exercises/src/main/kotlin/com/nrkei/training/oo/quantity/IntervalQuantity.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/quantity/IntervalQuantity.kt
@@ -1,9 +1,11 @@
 package com.nrkei.training.oo.quantity
 
+import com.nrkei.training.oo.orderable.Orderable
 import kotlin.math.absoluteValue
 
 // Understands a specific measurement
-open class IntervalQuantity internal constructor(amount: Number, protected val unit: Unit) {
+open class IntervalQuantity internal constructor(amount: Number, protected val unit: Unit)
+    : Orderable<IntervalQuantity> {
     protected val amount = amount.toDouble()
 
     @Suppress("ComplexMethod")
@@ -17,4 +19,6 @@ open class IntervalQuantity internal constructor(amount: Number, protected val u
     internal fun convertedAmount(other: IntervalQuantity) = this.unit.convertedAmount(other.amount, other. unit)
 
     override fun hashCode() = unit.hashCode(amount)
+
+    override fun isBetterThan(other: IntervalQuantity) = this.amount > convertedAmount(other)
 }
