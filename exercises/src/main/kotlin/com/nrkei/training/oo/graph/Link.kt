ec6820ab6542d12441da4bd411368ce82e25961e
Fred George <fredgeorge@acm.org>
2021-08-10 15:18:15 +0200

    Fred: Graph with cost() and hopCount() using path() with strategies

--- exercises/src/main/kotlin/com/nrkei/training/oo/graph/Link.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/graph/Link.kt
@@ -3,17 +3,9 @@ package com.nrkei.training.oo.graph
 // Understands a connection from one Node to another Node
 internal class Link(private val target: Node, private val cost: Double) {
     companion object {
-        internal val LEAST_COST = { cost: Double -> cost }
-        internal val FEWEST_HOPS = { _: Double -> 1.0 }
-
         internal fun List<Link>.totalCost() = this.sumOf { it.cost }
     }
-
-    internal fun cost(destination: Node, visitedNodes: List<Node>, strategy: CostStrategy) =
-        target.cost(destination, visitedNodes, strategy) + strategy(cost)
 
     internal fun path(destination: Node, visitedNodes: List<Node>, strategy: PathStrategy) =
         target.path(destination, visitedNodes, strategy).also { it.prepend(this) }
 }
-
-internal typealias CostStrategy = (Double) -> Double
--- exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
@@ -1,37 +1,23 @@
 package com.nrkei.training.oo.graph
 
-import com.nrkei.training.oo.graph.Link.Companion.FEWEST_HOPS
-import com.nrkei.training.oo.graph.Link.Companion.LEAST_COST
 import com.nrkei.training.oo.graph.Path.ActualPath
 
 class Node {
-    companion object {
-        private const val UNREACHABLE = Double.POSITIVE_INFINITY
-    }
-
     private val links = mutableListOf<Link>()
 
     infix fun canReach(destination: Node) = path(destination, noVisitedNodes, Path::cost) is ActualPath
 
-    infix fun hopCount(destination: Node) = cost(destination,FEWEST_HOPS).toInt()
+    infix fun hopCount(destination: Node) = path(destination, Path::hopCount).hopCount()
 
     infix fun cost(destination: Node) = path(destination).cost()
 
-    private fun cost(destination: Node, strategy: CostStrategy) =
-        cost(destination, noVisitedNodes, strategy).also { result ->
-            require(result != UNREACHABLE) { "Destination is unreachable" }
-        }
+    infix fun path(destination: Node) = path(destination, Path::cost)
 
-    internal fun cost(destination: Node, visitedNodes: List<Node>, strategy: CostStrategy): Double {
-        if (this == destination) return 0.0
-        if (this in visitedNodes || links.isEmpty()) return UNREACHABLE
-        return links.minOf { link -> link.cost(destination, visitedNodes + this, strategy) }
-    }
+    private fun path(destination: Node, strategy: PathStrategy) =
+        path(destination, noVisitedNodes, strategy)
+            .also { result -> require(result is ActualPath) { "Destination is unreachable" } }
 
-    infix fun path(destination: Node) = path(destination, noVisitedNodes, Path::cost).also { result ->
-        require(result is ActualPath) { "Destination is unreachable" }
-    }
-
+    @Suppress("ComplexMethod")
     internal fun path(destination: Node, visitedNodes: List<Node>, strategy: PathStrategy): Path {
         if (this == destination) return ActualPath()
         if (this in visitedNodes) return Path.None
